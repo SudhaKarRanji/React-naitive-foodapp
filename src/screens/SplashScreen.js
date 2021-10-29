94e222e45b463d125c0ec7a9fbfe47f0ec0247ca
SudhakarRanji <sudha0523@gmail.com>
2021-10-29 15:24:49 +0530

    First Commit and splash screen created

---
+++ src/screens/SplashScreen.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Image, StatusBar, StyleSheet, Text, View} from 'react-native';
+import {Colors, Fonts, Images} from '../constants';
+
+const SplashScreen = () => {
+  return (
+    <View style={styles.container}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={Colors.DEFAULT_GREEN}
+        translucent
+      />
+      <Image
+       source={Images.PLATE}
+       resizeMode="contain"
+       style={styles.image} />
+      <Text style={styles.titletext}>Food Delivery</Text>
+    </View>
+  );
+};
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.DEFAULT_GREEN,
+  },
+  image:{
+      height: 100,
+      width: 150,
+  },
+  titletext:{
+        fontSize: 32,
+        color: Colors.DEFAULT_WHITE,
+        fontFamily:Fonts.POPPINS_THIN,
+  }
+});
